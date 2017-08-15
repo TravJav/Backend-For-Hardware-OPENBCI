2fd7d1db14d78bbc9ecf49fc83e6d1ce87eb7a6d
TravJav <travishaycock92@gmail.com>
2017-08-15 17:09:05 -0400

    sending

--- read.js
+++ read.js
@@ -1,35 +1,51 @@
 
  // import from ganglion npm file to get raw EEG data
-import io from WebSocket.io;
-import bluetooth_stream from 'scan.js';
+//import io from WebSocket.io;
+const Bluetooth = require('./scan.js');
+//const express = require('express'),
+//app = express()
+//server = require('http').createServer(app),
+//io = require('socket.io')(server)
+//const socket = io("");
 
-const express = require('express'),
-app = express(),
-server = require('http').createServer(app),
-io = require('socket.io')(server)
-const socket = io("");
+/*
+NAVIGATION:
 
+ ATT: bluetooth_stream is the function being passed in the scan.js class which is inputting the microvolts in an Array of 4 objects
 
-
-
-
-class Transfer {
-
-
+*/
+//class Transfer {
+/*
 constructor(){
-    super();
+
 this.bluetooth_stream = bluetooth_stream;
 
-    
+
+}
+*/
+/*
+Pass in the data to function
+*/
+
+
+function input_data() {
+
+console.log("-------------------------------+ ");
+ Bluetooth. bluetoothData();
+
+
 }
 
-input_data(bluetooth_stream){
-bluetooth_stream
+
+input_data();
+
+
+
+    
    
 
 
 
-}
 
 
 
@@ -38,4 +54,12 @@ bluetooth_stream
 
 
 
-}
+
+
+
+/*
+server.listen(8080, ()=>{
+    console.log('Listening For  Client COnnection');
+})
+
+*/
--- scan.js
+++ scan.js
@@ -5,42 +5,47 @@
 
 const Ganglion = require('openbci-ganglion').Ganglion;
 const ganglion = new  Ganglion();
-
-
-
-
-
-
-
-/*
+/*================================================================================================================================================================================
+=============================================================================================================================================================================================================================
 This function will take in the RAW EEG, EMG, ECG data and send the Array with 4 objects inside ( Microvolts) to the 
 read.js class 
 
-*/
+=============================================================================================================================================================================================================================
+==============================================================================================================================================================================*/
  function  bluetooth_stream() {
 
-          ganglion.once('ganglionFound', (peripheral) => {
-          // Stop searching for BLE devices once a ganglion is found.
-          ganglion.searchStop();
-          ganglion.on('sample', (sample) => {
-              /** Work with sample */
-          console.log(sample.sampleNumber);
-              for (let i = 0; i < ganglion.numberOfChannels(); i++) {
-              // Where the microvolts will be outputedc
-                console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
-              }
-            });
-              ganglion.once('ready', () => {
-              ganglion.streamStart();
-          });
-             ganglion.connect(peripheral);
-       });
-            // Start scanning for BLE devices
-            ganglion.searchStart();
+ganglion.once('ganglionFound', (peripheral) => {
+// Stop searching for BLE devices once a ganglion is found.
+ganglion.searchStop();
+ganglion.on('sample', (sample) => {
+/** Work with sample */
+console.log(sample.sampleNumber);
+for (let i = 0; i < ganglion.numberOfChannels(); i++) {
+// Where the microvolts will be outputed
+  console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
+}
+});
+
+
+ganglion.once('ready', () => {
+ganglion.streamStart();
+});
+ganglion.connect(peripheral);
+});
+
+
+// Start scanning for BLE devices
+ganglion.searchStart();
 }
 
 
 
+// function that will be accessible
 
-bluetooth_stream();
-module.exports =  bluetooth_stream; 
+
+module.exports = {
+
+  bluetoothData : bluetooth_stream
+
+
+}
