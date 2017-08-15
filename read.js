59df63e8f32f1d8a0dccc74279b5f9ee5454e87f
TravJav <travishaycock92@gmail.com>
2017-08-15 16:05:20 -0400

    adding data

--- read.js
+++ read.js
@@ -1,2 +1,41 @@
-//import {scan} from scan;
 
+ // import from ganglion npm file to get raw EEG data
+import io from WebSocket.io;
+import bluetooth_stream from 'scan.js';
+
+const express = require('express'),
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io')(server)
+const socket = io("");
+
+
+
+
+
+class Transfer {
+
+
+constructor(){
+    super();
+this.bluetooth_stream = bluetooth_stream;
+
+    
+}
+
+input_data(bluetooth_stream){
+bluetooth_stream
+   
+
+
+
+}
+
+
+
+
+
+
+
+
+}
--- scan.js
+++ scan.js
@@ -1,37 +1,46 @@
 
 
 
+// libs
 
 const Ganglion = require('openbci-ganglion').Ganglion;
-const ganglion = new Ganglion();
-
-
-
-    ganglion.once('ganglionFound', (peripheral) => {
-      // Stop searching for BLE devices once a ganglion is found.
-      ganglion.searchStop();
+const ganglion = new  Ganglion();
 
 
 
 
-    ganglion.on('sample', (sample) => {
-          /** Work with sample */
-       console.log(sample.sampleNumber);
-          for (let i = 0; i < ganglion.numberOfChannels(); i++) {
-           // Where the microvolts will be outputedc
-            console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
-          }
-        });
-
-
-  ganglion.once('ready', () => {
-    ganglion.streamStart();
-  });
-  ganglion.connect(peripheral);
-});
-// Start scanning for BLE devices
-ganglion.searchStart();
 
 
 
-//export default scan; // export the data place into new file
+/*
+This function will take in the RAW EEG, EMG, ECG data and send the Array with 4 objects inside ( Microvolts) to the 
+read.js class 
+
+*/
+ function  bluetooth_stream() {
+
+          ganglion.once('ganglionFound', (peripheral) => {
+          // Stop searching for BLE devices once a ganglion is found.
+          ganglion.searchStop();
+          ganglion.on('sample', (sample) => {
+              /** Work with sample */
+          console.log(sample.sampleNumber);
+              for (let i = 0; i < ganglion.numberOfChannels(); i++) {
+              // Where the microvolts will be outputedc
+                console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
+              }
+            });
+              ganglion.once('ready', () => {
+              ganglion.streamStart();
+          });
+             ganglion.connect(peripheral);
+       });
+            // Start scanning for BLE devices
+            ganglion.searchStart();
+}
+
+
+
+
+bluetooth_stream();
+module.exports =  bluetooth_stream; 
