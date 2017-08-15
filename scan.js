f9aaf746f9f08bcfaaa7305b67a1478186fa8da0
TravJav <travishaycock92@gmail.com>
2017-08-15 13:04:00 -0400

    first commit

---
+++ scan.js
@@ -0,0 +1,37 @@
+
+
+
+
+const Ganglion = require('openbci-ganglion').Ganglion;
+const ganglion = new Ganglion();
+
+
+
+    ganglion.once('ganglionFound', (peripheral) => {
+      // Stop searching for BLE devices once a ganglion is found.
+      ganglion.searchStop();
+
+
+
+
+    ganglion.on('sample', (sample) => {
+          /** Work with sample */
+       console.log(sample.sampleNumber);
+          for (let i = 0; i < ganglion.numberOfChannels(); i++) {
+           // Where the microvolts will be outputedc
+            console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
+          }
+        });
+
+
+  ganglion.once('ready', () => {
+    ganglion.streamStart();
+  });
+  ganglion.connect(peripheral);
+});
+// Start scanning for BLE devices
+ganglion.searchStart();
+
+
+
+//export default scan; // export the data place into new file
