f2504f7d98a73df956a936f3d45af49297731855
TravJav <travishaycock92@gmail.com>
2017-08-17 14:04:09 -0400

    new layout single file

--- scan.js
+++ scan.js
@@ -1,21 +1,27 @@
 
 
-
+const Bluetooth = require('./scan.js');
+const express = require('express'),
+app = express(),
+server = require('http').createServer(app);
+var io = require('socket.io')(server);
 // libs
 
 const Ganglion = require('openbci-ganglion').Ganglion;
 const ganglion = new  Ganglion();
 
 
+server.listen(8080, ()=>{
+  console.log('Listening For  Client COnnection');
+});
 
+io.on('connection', socket=>{
+  console.log("\n Client has connected to our server");
+ganglion.searchStart();
 
+  });
 
-
-let GanglionChannels = ganglion.numberOfChannels();
-let GanglionObjects;
-
-
-
+  
 /*================================================================================================================================================================================
 =============================================================================================================================================================================================================================
 This function will take in the RAW EEG, EMG, ECG data and send the Array with 4 objects inside ( Microvolts) to the 
@@ -23,45 +29,51 @@ read.js class
 
 =============================================================================================================================================================================================================================
 ==============================================================================================================================================================================*/
- function  bluetooth_stream() {
 
-ganglion.once('ganglionFound', (peripheral) => {
-// Stop searching for BLE devices once a ganglion is found.
-ganglion.searchStop();
-ganglion.on('sample', (sample) => {
-/** Work with sample */
-console.log(sample.sampleNumber);
-for (let i = 0; i < ganglion.numberOfChannels(); i++) {
-// Where the microvolts will be outputed
-  console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
 
- GanglionObjects = sample.channelData[i].toFixed(8);
+
+
+
+
+
+
+    ganglion.once('ganglionFound', (peripheral) => {
+    // Stop searching for BLE devices once a ganglion is found.
+    ganglion.searchStop();
+    ganglion.on('sample', (sample) => {
+    /** Work with sample */
+    console.log(sample.sampleNumber);
+    for (let i = 0; i < ganglion.numberOfChannels(); i++) {
+    // Where the microvolts will be outputed
+      console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
+    
+     GanglionObjects = sample.channelData[i].toFixed(8);
+     pass_data(GanglionObjects);
+    }
+    });
+
+    ganglion.once('ready', () => {
+      ganglion.streamStart();
+    });
+    ganglion.connect(peripheral);
+
+  });
+
+
+  
+
+  
+  
+/*
+Function to actually pass the data from the socket to the front end
+
+*/
+
+
+function pass_data(GanglionObjects){
+
+console.log('socket has been connected');
+io.emit("arraytransfer",GanglionObjects);
 
 }
-});
 
-
-ganglion.once('ready', () => {
-ganglion.streamStart();
-});
-ganglion.connect(peripheral);
-});
-
-
-// Start scanning for BLE devices
-ganglion.searchStart();
-}
-
-
-
-// function that will be accessible
-
-
-module.exports = {
-
-  bluetoothData : bluetooth_stream,
-  GanglionData : GanglionObjects,
-  ChannelSize: GanglionChannels  
-
-
-}
